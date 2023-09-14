4ed3ece6ad7a17b1f0e28c0524200045cf5521f1
jihun1.kim <jihun.kim@mnc.ai>
2023-09-14 07:37:24 +0000

    add result concoat code

---
+++ concat_results.py
@@ -0,0 +1,22 @@
+import json
+import sys
+import os
+result_path = sys.argv[1]
+
+_result = {'model_name':result_path, 'arc_challenge':-99, 'hellaswag':-99, 'mmlu':-99, 'truthfulqa_mc':-99}
+
+with open(os.path.join(result_path, 'arc_result.json'), 'r', encoding='utf-8-sig') as file:
+    _result['arc_challenge'] = round(json.load(file)['results']['arc_challenge']['acc_norm']*100,2)
+
+with open(os.path.join(result_path, 'hellaswag_result.json'), 'r', encoding='utf-8-sig') as file:
+    _result['hellaswag'] = round(json.load(file)['results']['hellaswag']['acc_norm']*100,2)
+
+with open(os.path.join(result_path, 'mmlu_result.json'), 'r', encoding='utf-8-sig') as file:
+    res_mmlu = json.load(file)
+acc_norm_values = [category_data['acc_norm'] for category_data in res_mmlu['results'].values()]
+_result['mmlu'] = round(sum(acc_norm_values) / len(acc_norm_values)*100,2)
+
+with open(os.path.join(result_path, 'truthfulQA_result.json'), 'r', encoding='utf-8-sig') as file:
+    _result['truthfulqa_mc'] = round(json.load(file)['results']['truthfulqa_mc']['mc2']*100,2)
+
+print(_result)
